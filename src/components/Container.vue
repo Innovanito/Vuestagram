c6c177ae6b07a1e9e31db90d991045880316c498
Innovanito <khg05311@naver.com>
2021-10-26 13:48:23 +0900

    first save file

---
+++ src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <Post :data="data[0]" />
+  <Post :data="data[1]" />
+  <Post :data="data[2]" />
+</template>
+
+<script>
+import Post from './Post.vue'
+export default {
+  components: {
+    Post
+  },
+  props: {
+    data : Array
+  }
+}
+</script>
+
+<style>
+
+</style>
